bc0cac41c937c5767b0399ac23cdc446c532af5c
isaacs <i@foohack.com>
2009-03-05 14:26:52 -0800

    Export the params a little slicker.  (WOW bash is ugly.)

--- curl.sh
+++ curl.sh
@@ -8,7 +8,7 @@ base_url=http://create.awe.sm/url.json
 #override these settings in awe.conf
 api_key=6c8b1a212434c2153c2f2c2f2c165a36140add243bf6eae876345f8fd11045d3
 version=1
-share_type=bash
+share_type=other
 create_type=shell_script
 callback=
 
@@ -27,5 +27,11 @@ if ! [ "$valid" == "HTTP/1.1 200" ]; then
 	exit 1
 fi
 
+postdata=""
+for i in callback target version share_type create_type api_key; do
+	postdata="$postdata&$i=$( eval 'echo $'"$i" )"
+done
+postdata=${postdata:1}
+
 # do it.
-curl -i -d "callback=$callback&target=$target&version=$version&share_type=$share_type&create_type=$create_type&api_key=$api_key" $base_url
+echo curl -i -d "$postdata" $base_url
